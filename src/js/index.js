a739784213bd5d9e475ec7d329280cab4990cf3b
EzequiasCalvo <calvo.ezequias@gmail.com>
2020-04-05 12:53:14 -0300

    archivo base.js para query selectors, obtener inputs y renderizar los resultados

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,13 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
 
 // Creamos nuestro objeto state. Para almacenar nuestra data y sus cambios.
 const state = {};
 const controlSearch = async () => {
     // 1) Get query from view
-    const query = 'pizza';
+    const query = searchView.getInput();
+    console.log(query);
 
     if (query) {
     
@@ -12,16 +15,18 @@ const controlSearch = async () => {
         state.search = new Search(query);
     
         // 3) Prepare UI for results
-        
+        searchView.clestInput();
+        searchView.clearResults();
+
         // 4) Search for recipes
         await state.search.getResults();
 
         // 5) Render results on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault() //Prevenimos acciones nativas. En este caso no queremos que se recargue la pagina al darle submit.
     controlSearch();
 });
---
+++ src/js/views/base.js
@@ -0,0 +1,6 @@
+export const elements = {
+    // Object with querySelectors
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchResList: document.querySelector('.results__list')
+};
