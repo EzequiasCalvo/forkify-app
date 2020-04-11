e435c92e5cff2c86e55314d73158d38541704cd9
EzequiasCalvo <calvo.ezequias@gmail.com>
2020-04-11 15:42:48 -0300

    Likes model and controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as listView from './views/listView';
 import * as recipeView from './views/recipeView';
@@ -130,6 +131,43 @@ elements.shopping.addEventListener('click', e => {
     }
 });
 
+// Like controller
+const controlLike = () => {
+    if (!state.likes) state.likes = new Likes();
+    const currentID = state.recipe.id;
+
+    // User has not yet liked current recipe
+    if (!state.likes.isLiked(currentID)) {
+        // Add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        );
+        // Toggle the like button
+        //likesView.toggleLikeBtn(true);
+
+        // Add like to UI list
+        //likesView.renderLike(newLike);
+        console.log(state.likes);
+
+    // User HAS liked current recipe
+    } else {
+        // Remove like from the state
+        state.likes.deleteLike(currentID);
+
+        // Toggle the like button
+        //likesView.toggleLikeBtn(false);
+
+        // Remove like from UI list
+        //likesView.deleteLike(currentID);
+         console.log(state.likes);
+    }
+    //likesView.toggleLikeMenu(state.likes.getNumLikes());
+};
+
+
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
@@ -146,5 +184,8 @@ elements.recipe.addEventListener('click', e => {
     } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
         // Add ingredients to shopping list
         controlList();
+    } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+        // Like controller
+        controlLike();
     }
 });
